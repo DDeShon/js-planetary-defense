ab4ae82805da4b3845ea1e1496c1e11313ae9d09
DDeShon <36036284+DDeShon@users.noreply.github.com>
2023-10-30 19:24:06 -0500

    working on planet class

--- script.js
+++ script.js
@@ -1,6 +1,23 @@
+class Planet {
+  constructor() {
+    this.x = 200;
+    this.y = 200;
+    this.radius = 80;
+  }
+
+  draw(context) {
+    context.beginPath();
+    context.ard(this.x, this.y, this.radius, 0, Math.PI * 2);
+    context.fill();
+  }
+}
+
 window.addEventListener("load", function () {
   const canvas = document.getElementById("canvas1");
   const ctx = canvas.getContext("2d");
   canvas.width = 800;
   canvas.height = 800;
+
+  const planet = new Planet();
+  planet.draw();
 });
