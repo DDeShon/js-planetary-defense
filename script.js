8ec9c48cec72a814c11a1cdff91ec4b5614baebb
DDeShon <36036284+DDeShon@users.noreply.github.com>
2023-10-30 19:36:52 -0500

    working on planet class

--- script.js
+++ script.js
@@ -1,12 +1,14 @@
 class Planet {
   constructor(game) {
     this.game = game;
-    this.x = 400;
-    this.y = 400;
+    this.x = this.game.width * 0.5;
+    this.y = this.game.height * 0.5;
     this.radius = 80;
+    this.image = document.getElementById("planet");
   }
 
   draw(context) {
+    context.drawImage(this.image, this.x - 100, this.y - 100);
     context.beginPath();
     context.arc(this.x, this.y, this.radius, 0, Math.PI * 2);
     context.stroke();
