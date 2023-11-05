7ce1898d10350be30f41731fdaf1f9e2a3c538c6
DDeShon <36036284+DDeShon@users.noreply.github.com>
2023-11-04 23:43:18 -0500

    working on player class

--- script.js
+++ script.js
@@ -21,15 +21,15 @@ class Player {
     this.x = this.game.width * 0.5;
     this.y = this.game.height * 0.5;
     this.radius = 40;
+    this.image = document.getElementById("player");
   }
   draw(context) {
+    context.drawImage(this.image, this.x - this.radius, this.y - this.radius);
     context.beginPath();
     context.arc(this.x, this.y, this.radius, 0, Math.PI * 2);
     context.stroke();
   }
-  update() {
-    this.x++;
-  }
+  update() {}
 }
 
 class Game {
