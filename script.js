9ae9838d169b724af90fb0132881e8bbd7ceb2c4
DDeShon <36036284+DDeShon@users.noreply.github.com>
2023-11-09 22:30:53 -0600

    working on projectile class

--- script.js
+++ script.js
@@ -60,10 +60,27 @@ class Projectile {
     this.x;
     this.y;
     this.radius = 20;
+    this.speedX = 1;
+    this.speedY = 1;
     this.free = true;
   }
   start() {
     this.free = false;
+  }
+  reset() {
+    this.free = true;
+  }
+  draw(context) {
+    if (!this.free) {
+      context.beginPath();
+      context.arc(this.x, this.y, this.radius, 0, Math.PI * 2);
+    }
+  }
+  update() {
+    if (!this.free) {
+      this.x += this.speedX;
+      this.y += this.speedY;
+    }
   }
 }
 
