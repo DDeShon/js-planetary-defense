380808033ca3ba6e6d09429f65233f9e3aa2fb8a
DDeShon <36036284+DDeShon@users.noreply.github.com>
2023-11-12 22:23:55 -0600

    working on enemy class

--- script.js
+++ script.js
@@ -137,6 +137,7 @@ class Enemy {
     if (!this.free) {
       context.beginPath();
       context.arc(this.x, this.y, this.radius, 0, Math.PI * 2);
+      context.stroke();
     }
   }
   update() {
