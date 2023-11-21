54ad1a9c5544e0d51cf7b6ef34f6d196cd5c8fed
DDeShon <36036284+DDeShon@users.noreply.github.com>
2023-11-21 00:59:31 -0600

    working on player lives

--- script.js
+++ script.js
@@ -348,6 +348,9 @@ class Game {
     context.textAlign = "left";
     context.font = "30px Impact";
     context.fillText("Score: " + this.score, 20, 30);
+    for (let i = 0; i < this.lives; i++) {
+      context.fillRect(20 + 15 * i, 60, 10, 30);
+    }
     if (this.gameOver) {
       context.textAlign = "center";
       let message1;
